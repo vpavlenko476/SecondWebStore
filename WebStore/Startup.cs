0b41bb523a5120e3bb7a1efae22e88090fe921f5
v.pavlenko <vpavlenko476@gmail.com>
2020-06-23 20:14:35 +0300

    Add project files.

---
+++ WebStore/Startup.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();
+		}
+		
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			app.UseStaticFiles();
+			app.UseDefaultFiles();
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name:"default",
+					pattern:"{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}

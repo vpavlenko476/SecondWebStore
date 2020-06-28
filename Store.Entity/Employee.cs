1ec81aca9a4f78b50267401d3cd7ee4e2761fc80
v.pavlenko <vpavlenko476@gmail.com>
2020-06-28 15:02:13 +0300

    [add] Store.Entities

--- Store.Entity/Employee.cs
+++ Store.Entity/Employee.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Store.Entity;
+using System.ComponentModel.DataAnnotations;
 
-namespace Store.Domain
+namespace Store.Entities
 {
-	public class Employee
+	class Employee: BaseEntity
 	{
-		public int Id { get; set; }
+		[StringLength(50)]
 		public string FirstName { get; set; }
+		[StringLength(50)]
 		public string SecondName { get; set; }
+		[StringLength(50)]
 		public string Patronymic { get; set; }
 		public string Email { get; set; }
 		public string MobilePhone { get; set; }

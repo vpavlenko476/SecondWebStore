0d28abdb89d54962a879ca8fbcba9e13b8609fd7
v.pavlenko <vpavlenko476@gmail.com>
2020-06-29 20:34:25 +0300

    перенос сотрудников в отдельный контролер

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -8,23 +8,21 @@ using System.Threading.Tasks;
 
 namespace WebStore.Controllers
 {
-	public class HomeController : Controller
+	public class EmployeeController : Controller
 	{
 		private readonly IBaseRepo<EmployeeEntity> _employeeRepo;
 		private readonly IMapper _mapper;
-		public HomeController(IBaseRepo<EmployeeEntity> repo, IMapper mapper)
+		public EmployeeController(IBaseRepo<EmployeeEntity> repo, IMapper mapper)
 		{
 			_employeeRepo = repo;
 			_mapper = mapper;
 		}		
-		
 		public IActionResult Index() => View(_mapper.Map<IList<Employee>>(_employeeRepo.GetAll()));
-		public IActionResult Employees() => View(_mapper.Map<IList<Employee>>(_employeeRepo.GetAll()));
 
-		public async Task<IActionResult> EmployeeInfoAsync(int id)			
+		public async Task<IActionResult> Details(int id)
 		{
 			var employee = _mapper.Map<Employee>(await _employeeRepo.GetOne(id));
 			return View(employee);
-		}
+		}		
 	}
 }

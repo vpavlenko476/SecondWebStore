3ec164eadb07525932bb8d71f32eb7417e0328ba
v.pavlenko <vpavlenko476@gmail.com>
2020-07-07 22:03:00 +0300

    модели представлений каталога и продукта

---
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Store.Entities.Abstract;
+
+namespace WebStore.ViewModels
+{
+	public class ProductViewModel: INamedEntity, IOrderedEntity
+	{
+		public string Name { get; set; }
+		public int Id { get; set; }
+		public int Order { get; set; }
+		public string ImageUrl { get; set; }
+		public decimal Price { get; set; }
+	}
+}

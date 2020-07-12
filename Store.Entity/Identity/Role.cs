4483678843de7c0d320a4178f3e7f4a75fe9a529
v.pavlenko <vpavlenko476@gmail.com>
2020-07-12 15:14:21 +0300

    [add] добавлены пользователь и роль

---
+++ Store.Entity/Identity/Role.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Store.Entities.Identity
+{
+	class Role: IdentityRole
+	{
+		/// <summary>
+		/// имя роли администратора
+		/// </summary>
+		public const string Administrator = "AdminRole";
+
+		/// <summary>
+		/// имя роли пользоватея
+		/// </summary>
+		public const string User = "UserRole";
+	}
+}
---
+++ Store.Entity/Identity/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Store.Entities.Identity
+{
+	class User : IdentityUser
+	{
+		/// <summary>
+		/// Имя пользователя администратора
+		/// </summary>
+		public const string Administrator = "Admin";
+		public const string DeafaultAdminPass = "Password0";
+		public string Description { get; set; }
+	}
+}

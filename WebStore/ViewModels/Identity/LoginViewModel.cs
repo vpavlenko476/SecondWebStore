e69ca243b2bd1ed10bbdb6dd8b9d682f777af17d
v.pavlenko <vpavlenko476@gmail.com>
2020-07-13 19:47:23 +0300

    Представления для входа и регистрации

---
+++ WebStore/ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels.Identity
+{
+	public class LoginViewModel
+	{
+		[Required]
+		[MaxLength(256)]
+		[Display(Name = "Имя пользователя")]		
+		public string UserName { get; set; }
+
+		[Required]
+		[Display(Name = "Пароль")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		[Display(Name = "Запомнить меня")]		
+		public bool RememberMe { get; set; }
+		
+		[HiddenInput(DisplayValue = false)]
+		public string ReturnUrl { get; set; }
+	}
+}
---
+++ WebStore/ViewModels/Identity/RegisterUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels.Identity
+{
+	public class RegisterUserViewModel
+	{
+		[Required(ErrorMessage = "Введите имя пользователя")]
+		[Display(Name = "Имя пользователя")]
+		[MaxLength(256)]
+		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Введите пароль")]
+		[Display(Name = "Пароль")]
+		[StringLength(20, MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Введите подтверждение пароля")]
+		[Display(Name = "Подтверждение пароля")]
+		[StringLength(20, MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password))]
+		public string PasswordConfirm { get; set; }
+	}
+}

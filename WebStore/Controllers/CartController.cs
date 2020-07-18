2fc2a2c0f6579855d1db1f4cef24d01eebbbb882
v.pavlenko <vpavlenko476@gmail.com>
2020-07-18 22:40:56 +0300

    Подготовка контроллера корзины

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Store.Services.Abstract;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {            
+            _cartService = cartService;
+        }
+
+        public IActionResult Details() => View(_cartService.GetCartModel());
+        
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,8 +5,7 @@ namespace WebStore.Controllers
 	public class HomeController : Controller
 	{		
 		public IActionResult Index() => View();
-		public IActionResult Blogs() => View();
-		public IActionResult Cart() => View();
+		public IActionResult Blogs() => View();		
 		public IActionResult ShopBlog() => View();
 		public IActionResult Checkout() => View();
 		public IActionResult Contacts() => View();

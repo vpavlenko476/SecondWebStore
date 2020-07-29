fb1301c9f4c0462c20a5f38cd229046173b8f0fa
v.pavlenko <vpavlenko476@gmail.com>
2020-07-29 22:16:25 +0300

    Заготовка для управление каталогом товаров

--- UI/WebStore/Areas/Admin/Controllers/ProductController.cs
+++ UI/WebStore/Areas/Admin/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Store.Domain;
 using Store.Entities.Identity;
 using Store.Services.Abstract;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebStore.Areas.Admin.Controllers
 {
@@ -24,5 +25,33 @@ namespace WebStore.Areas.Admin.Controllers
         {
             return View(_mapper.Map<List<Product>>(_productService.GetProducts()));
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product is null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid) return View(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product is null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {            
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
